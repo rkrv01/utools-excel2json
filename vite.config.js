975eb10e675cb8ad77f5fb408571986cc5490452
raorui <raorui@ganweiiot.com.cn>
2022-04-15 15:48:49 +0800

    基本路径调整、移除发布路径忽略

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/utools-excel2-json',
     plugins: [
         vue(),
         WindiCSS(),
