cda6524eb28775b4f1f67b5c936a1cbbb4da98c4
rui <raorui2020@foxmail.com>
2022-05-25 01:18:23 +0800

    style(): 💄 页面主题样式修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <n-notification-provider :max="3">
     <n-loading-bar-provider>
-      <View />
+      <View :theme-overrides="themeOverrides" />
     </n-loading-bar-provider>
   </n-notification-provider>
 </template>
@@ -9,5 +9,12 @@
 <script setup>
 
 import View from "./View/index.vue";
-// import View from "./View/Test/index.vue";
+
+
+/**主题配置 */
+const themeOverrides = {
+  common: {
+    primaryColor: "#25b39e",
+  },
+};
 </script>
--- src/View/index.vue
+++ src/View/index.vue
@@ -1,20 +1,16 @@
 <template>
   <n-config-provider :theme="theme">
-    <div class="p-20px h-100vh box-border">
-      <div class="app-card w-full h-full">
+    <div class="p-20px h-100vh box-border relative">
+      <div class="app-card  w-[calc(100%-10px)] h-[calc(100%-10px)]">
+        <!-- 顶部 上传文件用 -->
         <div class="card-header">
           <div class="uploader-container">
-            <n-upload
-              accept=".xls,.xlsx"
-              :default-upload="false"
-              :show-file-list="false"
-              :on-change="fileChangeHandler"
-              :theme-overrides="themeOverrides"
-            >
+            <n-upload accept=".xls,.xlsx" :default-upload="false" :show-file-list="false"
+              :on-change="fileChangeHandler">
               <n-upload-dragger>
                 <div class="mb-8px">
                   <n-icon size="30" :depth="3">
-                    <archive-icon />
+                    <archive-icon color="#25b39eb8" />
                   </n-icon>
                 </div>
                 <n-text class="text-size-[16px]">
@@ -30,105 +26,117 @@
 
         <n-divider />
 
+        <!-- 下方，结果输出展示用 -->
         <div class="result h-full flex flex-col">
           <div class="mb-5px flex justify-between">
             <div class="options-left flex items-center">
-              <label class="ml-10px">
-                json格式化
-                <n-switch
-                  v-model:value="isFormatter"
-                  :theme-overrides="themeOverrides"
-                  :disabled="!jsonValue"
-                />
-              </label>
-              <n-button
-                size="small"
-                strong
-                secondary
-                type="info"
-                class="copy-text-btn ml-50px"
-                :data-clipboard-text="jsonValue"
-                :disabled="!jsonValue"
-              >
+              <!-- <label class="ml-10px">
+                JSON格式化
+                <n-switch v-model:value="isFormatter" :disabled="!jsonValue" />
+              </label> -->
+
+                  <!-- 指定表名转换 -->
+              <n-select size="small" class="w-300px" clearable multiple placeholder="指定表名转换"
+                v-model:value="exportNameValue" :options="exportNameOptions" v-show="exportNameOptions.length" />
+
+            </div>
+
+            <div class="options-right flex items-center">
+          
+              <!-- 复制结果 -->
+              <n-button size="small" strong secondary  type="primary" class="copy-text-btn ml-10px" :data-clipboard-text="jsonValue"
+                :disabled="!jsonValue">
                 {{ copyBtnText }}
               </n-button>
-              
-              <n-button
-                size="small"
-                strong
-                secondary
-                type="warning"
-                class="ml-20px"
-                :disabled="!jsonValue"
-                @click="clearResultHandler"
-              >
+
+              <!--  清空结果 -->
+              <n-button size="small" strong secondary  type="error" class="ml-10px" :disabled="!jsonValue"
+                @click="clearResultHandler">
                 清空结果
               </n-button>
             </div>
-            <div class="options-right flex items-center">
-              <!-- <span>导出指定表</span> -->
-              <n-select
-                class="ml-10px w-300px"
-                clearable
-                multiple
-                placeholder="指定表名转换"
-                v-model:value="exportNameValue"
-                :options="exportNameOptions"
-                v-show="exportNameOptions.length"
-              />
-            </div>
           </div>
+
           <!-- 输出json代码 -->
           <!-- 压缩单行超过一万字符的代码不高亮显示 -->
-          <pre
-            v-if="!isFormatter && jsonValue.length > 10000"
-            class="json-container flex-1 overflow-y-auto"
-            >{{ jsonValue }}</pre
-          >
-          <HighLightJs
-            v-else
-            language="json"
-            :code="jsonValue"
-            class="json-container flex-1 overflow-y-auto"
-          />
+          <pre v-if="!isFormatter && jsonValue.length > 10000"
+            class="json-container flex-1 overflow-y-auto">{{ jsonValue }}</pre>
+          <HighLightJs v-else language="json" :code="jsonValue" class="json-container flex-1 overflow-y-auto" />
         </div>
       </div>
+
+      <n-icon size="20" class="setting-icon absolute right-10px bottom-10px cursor-pointer"
+       color="#25b39e"
+       @click="openSettingHandler">
+        <settingIcon class="text-size-(20px)" />
+      </n-icon>
     </div>
+
+    <!-- 设置抽屉 -->
+    <n-drawer v-model:show="showDrawer" :width="300" placement="right">
+      <n-drawer-content title="自定义">
+        <SettingModal ref="settingModalRef" />
+      </n-drawer-content>
+    </n-drawer>
   </n-config-provider>
 </template>
 
 <script setup>
-import { FileExcelTwotone as ArchiveIcon } from "@vicons/antd";
+// import { FileExcelTwotone as ArchiveIcon, SettingOutlined as settingIcon } from "@vicons/antd";
+import { SettingOutlined as settingIcon } from "@vicons/antd";
+import { CloudUploadSharp as ArchiveIcon } from "@vicons/ionicons5";
 import { useOsTheme, darkTheme } from "naive-ui";
+import { storeToRefs } from "pinia"
 
+import SettingModal from "./components/SettingModal.vue"
 import useCopy from "./useCopy";
 import useHeightLight from "./useHeightLight";
 import useReadExcel from "./useReadExcel";
 import useUtools from "./useUtools";
 import useShowJson from "./useShowJson";
 import useDark from "./useDark";
+import { useSetting } from "@/stores/setting"
+
+const {
+  // 自动格式化
+  jsonFormatt: isFormatter,
+} = storeToRefs(useSetting())
+
 
 const osThemeRef = useOsTheme();
 const theme = computed(() => (osThemeRef.value === "dark" ? darkTheme : null));
 
-// 自动格式化
-const isFormatter = ref(true);
 // 导出表名
 const exportNameValue = ref([]);
+
+// 设置抽屉
+const showDrawer = ref(false)
+const settingModalRef = ref();
+function openSettingHandler() {
+  showDrawer.value = true
+  nextTick(() => {
+
+    console.log("settingModalRef:", settingModalRef);
+    settingModalRef.value.initData()
+  })
+}
+
 const { isDark } = useDark();
 
 const { HighLightJs } = useHeightLight(isDark);
-const { copyBtnText, initClipboard } = useCopy();
-
-const { runFileRead, sheetNames, excelvalue, renderFileByNode,clearResult } =
+const { runFileRead, sheetNames, excelvalue, renderFileByNode, clearResult } =
   useReadExcel(exportNameValue);
 
-const { showMainWindow } = useUtools(
+const { showMainWindow, hideMainWindow } = useUtools(
   sheetNames,
   excelvalue,
   exportNameValue,
   renderFileByNode
 );
+
+const { copyBtnText, initClipboard } = useCopy(clearResult, hideMainWindow);
+
+
 
 const { jsonValue } = useShowJson(isFormatter, excelvalue, exportNameValue);
 
@@ -142,12 +150,7 @@ const exportNameOptions = computed(() => {
   });
 });
 
-/**主题配置 */
-const themeOverrides = {
-  common: {
-    primaryColor: "#25b39e",
-  },
-};
+
 
 const styles = computed(() => {
   const light = !isDark.value;
@@ -169,7 +172,7 @@ function fileChangeHandler(data) {
 }
 
 /**清空转换结果 */
-function clearResultHandler(){
+function clearResultHandler() {
   // 清空json最原始数据
   clearResult()
 }
@@ -193,13 +196,16 @@ onMounted(() => {
     overflow: hidden;
   }
 }
+
 //转换文件容器
 .uploader-container {
   :deep() {
     .n-upload-trigger {
       width: 100%;
+
       .n-upload-dragger {
         padding: 10px;
+        
       }
     }
   }
@@ -216,5 +222,14 @@ onMounted(() => {
 
   // height: 400px;
   // overflow-y: auto;
+}
+
+// 设置按钮
+.setting-icon {
+  transition: transform .5s;
+
+  &:hover {
+    transform: rotate(90deg);
+  }
 }
 </style>
