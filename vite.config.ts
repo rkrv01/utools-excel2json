068dae76301b5e078c77aba1c557cf00ed77758a
rui <raorui2020@foxmail.com>
2022-07-07 08:32:05 +0800

    自动引入配置

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         imports: ["vue"],
         // resolvers: [ElementPlusResolver()],
         resolvers: [],
+        dts: true,
     }),
     Components({
         resolvers: [
